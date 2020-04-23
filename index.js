2489fd3997579af9022fa077934b9891ee27c4fb
Jeff Jiang <jeffjiang8@gmail.com>
2020-04-23 19:26:12 -0400

    2.0

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ function steps(n, row = 0, stair = '') {
 
     steps(n, row, stair)
 }
+
+// OR
+
+// function steps(n) {
+    // for ( let row = 0; row < n; row++ ){
+    //     let stair = ''
+    //     for ( let column = 0; column < n; column++ ) {
+    //         if ( column <= row) {
+    //             stair += '#'
+    //         } else {
+    //             stair += ' '
+    //         }
+    //     }
+    //     console.log(stair)
+    // }
+// }
